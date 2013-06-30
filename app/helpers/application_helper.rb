735b2045b8e0d2cb08e89f267e205265c9eda667
Cristian Carreño <kiel@acid.cl>
2013-06-30 04:18:56 -0400

    Add layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+	def carousel_images
+    [ {title: 'Naturaleza1', info: 'Ejemplo 1', name: 'naturaleza.jpg'},
+      {title: 'Naturaleza2', info: 'Ejemplo 2', name: 'naturaleza2.jpg'}]
+	end
 end
